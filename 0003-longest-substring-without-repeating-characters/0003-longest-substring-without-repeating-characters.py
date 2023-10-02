fea34091847424c1c896cac40e712aea538cac4b
AKSHAY ARJUN <68991993+Akshay-Arjun@users.noreply.github.com>
2023-10-02 20:10:01 +0530

    Time: 17 ms (99.43%), Space: 14.1 MB (37.57%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_index = {}  # to store the index of each character
+        start = 0  # start index of the current substring
+        max_length = 0  # length of the longest substring without repeating characters
+
+        for end in range(len(s)):
+            if s[end] in char_index and char_index[s[end]] >= start:
+                start = char_index[s[end]] + 1
+
+            char_index[s[end]] = end
+            max_length = max(max_length, end - start + 1)
+
+        return max_length
